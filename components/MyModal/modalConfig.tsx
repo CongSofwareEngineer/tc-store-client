ab2229b3e7672dc289794933f618b19848258adc
HoDienCong <hodiencong2000@gmail.com>
2024-06-11 23:23:57 +0700

    fix build

--- components/MyModal/modalConfig.tsx
+++ components/MyModal/modalConfig.tsx
@@ -3,24 +3,24 @@ import { ModalPropsType } from './type'
 import { Modal } from 'antd'
 
 const ModalConfig = ({ config, closeModal }: ModalPropsType) => {
-  return (
-    config?.content && (
-      <Modal
-        title={config?.title || <></>}
-        open={config?.open || false}
-        onCancel={closeModal}
-        footer={<></>}
-        centered
-        {...config}
-        closable={config.showBtnClose}
-        keyboard={config?.overClickClose}
-        maskClosable={config?.overClickClose}
-      >
-        <div className={`w-full h-full ${config?.classContent}`}>
-          {config?.content}
-        </div>
-      </Modal>
-    )
+  return config?.content ? (
+    <Modal
+      title={config?.title || <></>}
+      open={config?.open || false}
+      onCancel={closeModal}
+      footer={<></>}
+      centered
+      {...config}
+      closable={config.showBtnClose}
+      keyboard={config?.overClickClose}
+      maskClosable={config?.overClickClose}
+    >
+      <div className={`w-full h-full ${config?.classContent}`}>
+        {config?.content}
+      </div>
+    </Modal>
+  ) : (
+    <></>
   )
 }
 
