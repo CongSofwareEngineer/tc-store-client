0e7461ae6ef1d6515712816a6b74fa821a6b5a10
ConggBaccor <cong@baccor>
2024-06-13 10:29:44 +0700

    fix btn back

--- components/BtnBack/index.tsx
+++ components/BtnBack/index.tsx
@@ -10,50 +10,43 @@ type BtnBackType = {
   title?: string[] | string
 }
 
-const BtnBack = ({
-  title,
-  url = [],
-  onClick = null,
-  children = undefined,
-}: BtnBackType) => {
+const BtnBack = ({ title, url = [], onClick = null }: BtnBackType) => {
   const router = useRouter()
   return (
-    children || (
-      <div className="flex w-full align-middle justify-start gap-1 mb-3 md:mb-6 ">
-        <MyImage
-          onClick={() => (onClick ? onClick() : router.back())}
-          src={images.icon.iconBack}
-          width={25}
-          height={25}
-          alt={'TC Store Icon Back page '}
-          className="cursor-pointer"
-        />
-        <div className="ml-2 flex gap-1">
-          {typeof title === 'string' ? (
-            <div className="md:text-[16px] text-[14px]">{title}</div>
-          ) : (
-            title?.map((item, index) => {
-              if (url[index]) {
-                return (
-                  <Link
-                    className="cursor-pointer hover:underline text-[16px] text-blue-700"
-                    href={url[index]}
-                    key={item}
-                  >
-                    {item} <RightOutlined className="black" />
-                  </Link>
-                )
-              }
+    <div className="flex w-full align-middle justify-start gap-1 mb-3 md:mb-6 ">
+      <MyImage
+        onClick={() => (onClick ? onClick() : router.back())}
+        src={images.icon.iconBack}
+        width={25}
+        height={25}
+        alt={'TC Store Icon Back page '}
+        className="cursor-pointer"
+      />
+      <div className="ml-2 flex gap-1">
+        {typeof title === 'string' ? (
+          <div className="md:text-[16px] text-[14px]">{title}</div>
+        ) : (
+          title?.map((item, index) => {
+            if (url[index]) {
               return (
-                <div className="md:text-[16px] text-[14px]" key={item}>
-                  {item}
-                </div>
+                <Link
+                  className="cursor-pointer hover:underline text-[16px] text-blue-700"
+                  href={url[index]}
+                  key={item}
+                >
+                  {item} <RightOutlined className="black" />
+                </Link>
               )
-            })
-          )}
-        </div>
+            }
+            return (
+              <div className="md:text-[16px] text-[14px]" key={item}>
+                {item}
+              </div>
+            )
+          })
+        )}
       </div>
-    )
+    </div>
   )
 }
 
