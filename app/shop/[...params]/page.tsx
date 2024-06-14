61ba9e5b2aec10785509d1e92b68b31a0040a484
ConggBaccor <cong@baccor>
2024-06-14 13:53:19 +0700

    fix build

--- app/shop/[...params]/page.tsx
+++ app/shop/[...params]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { PropsWithChildren, useState } from 'react'
+import React, { useState } from 'react'
 import { ItemDetailType } from './type'
 import PrimaryButton from '@/components/PrimaryButton'
 import useLanguage from '@/hook/useLanguage'
