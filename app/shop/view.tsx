fd6813c4fc31f810f7b84d0b4462e507e838b8dc
ConggBaccor <cong@baccor>
2024-06-28 16:05:39 +0700

    fix build

--- app/shop/view.tsx
+++ app/shop/view.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import MenuCategory from './Component/MenuCategory'
 import ItemProduct from '@/components/ItemProduct'
 import { Input } from 'antd'
