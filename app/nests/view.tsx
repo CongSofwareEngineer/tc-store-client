6d5c294eed4205cb0493e155d8b7188084738ecb
HoDienCong <hodiencong2000@gmail.com>
2024-07-20 19:05:08 +0700

    fix build

--- app/nests/view.tsx
+++ app/nests/view.tsx
@@ -26,10 +26,10 @@ const PageNestsScreen = () => {
         <Spin />
       ) : (
         <div className="w-full grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 md:gap-3 gap-2">
-          {data?.data.map((e, index) => {
+          {data?.map((e, index) => {
             return <ItemNest data={e} key={`item-nests-${index}`} />
           })}
-          {data?.data.length === 0 && <div>{translate('warning.noData')}</div>}
+          {data?.length === 0 && <div>{translate('warning.noData')}</div>}
         </div>
       )}
     </div>
