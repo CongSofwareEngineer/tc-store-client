206bb63cb91885d50a04226a6cc04201f5790689
ConggBaccor <cong@baccor>
2024-09-11 11:28:40 +0700

    fix ui

--- app/admin/Component/Container/index.tsx
+++ app/admin/Component/Container/index.tsx
@@ -97,21 +97,23 @@ const ContainerAdmin = ({ children }: { children: React.ReactNode }) => {
               })}
             </div>
           ) : (
-            <div className="flex w-full gap-4  overflow-x-auto  pt-4 pb-2 px-5 ">
-              {LIST_MENU.map((e) => {
-                return (
-                  <div key={e.url} className="w-auto">
-                    <Link
-                      href={e.url}
-                      className={`${
-                        patchName === e.url ? 'font-bold underline' : ''
-                      } text-nowrap bg-green-100 p-4 py-1 min-w-[50px] border-[1px] border-blue-300 rounded-xl text-black`}
-                    >
-                      {e.title}
-                    </Link>
-                  </div>
-                )
-              })}
+            <div className="flex w-full gap-4   pt-4  px-5 ">
+              <div className="flex w-full gap-4  overflow-x-auto  pt-4 pb-3 px-5 ">
+                {LIST_MENU.map((e) => {
+                  return (
+                    <div key={e.url} className="w-auto">
+                      <Link
+                        href={e.url}
+                        className={`${
+                          patchName === e.url ? 'font-bold underline' : ''
+                        } text-nowrap bg-green-100 p-4 py-1 min-w-[50px] border-[1px] border-blue-300 rounded-xl text-black`}
+                      >
+                        {e.title}
+                      </Link>
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           )}
           <div className="flex flex-1 md:p-4 p-5 md:max-w-[calc(100vw-212px)]">
