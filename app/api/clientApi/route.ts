1d9d6f12267b7ce634acc92bbfe19c912e915c5b
bacoor <ccong@bacoor.co>
2024-11-15 14:18:50 +0700

    fix build

--- app/api/clientApi/route.ts
+++ app/api/clientApi/route.ts
@@ -1,8 +1,6 @@
-
-
-import { FB_FC } from '@/constant/firebase';
-import { FirebaseConfig } from '@/services/firebaseService';
-import { formatResponseDataServer, pareDataClient } from '@/utils/serverNext';
+import { FB_FC } from '@/constant/firebase'
+import { FirebaseServices } from '@/services/firebaseService'
+import { formatResponseDataServer, pareDataClient } from '@/utils/serverNext'
 
 export async function POST(req: any) {
   try {
@@ -12,45 +10,48 @@ export async function POST(req: any) {
       return new Response('You are hacker', { status: 500 })
     }
 
-    const dataFB = FirebaseConfig.createFBFun(bodyDecode.nameDB)
+    const dataFB = FirebaseServices.createFBFun(bodyDecode.nameDB)
     let dataRequest: any
 
     switch (bodyDecode.namFn) {
       case FB_FC.queryListData:
-        dataRequest = await dataFB.listQueryData(bodyDecode.body[FB_FC.queryListData])
-        break;
+        dataRequest = await dataFB.listQueryData(
+          bodyDecode.body[FB_FC.queryListData]
+        )
+        break
       case FB_FC.updateData:
-        dataRequest = await dataFB.updateData(bodyDecode.body.id, bodyDecode.body.data)
-        break;
+        dataRequest = await dataFB.updateData(
+          bodyDecode.body.id,
+          bodyDecode.body.data
+        )
+        break
       case FB_FC.addData:
         dataRequest = await dataFB.addData(bodyDecode.body.data)
-        break;
+        break
       case FB_FC.getAllDataOption2:
         dataRequest = await dataFB.getDataOption2(
           bodyDecode.body.data?.dataLast,
           bodyDecode.body.data?.keyOderBy,
           bodyDecode.body.data?.limitPage
         )
-        break;
+        break
       case FB_FC.queryDataOption2:
         dataRequest = await dataFB.getDataOption2(
           bodyDecode.body.data?.dataLast,
           bodyDecode.body.data?.keyOderBy,
           bodyDecode.body.data?.limitPage
         )
-        break;
+        break
       case FB_FC.getDataByID:
         dataRequest = await dataFB.getDataByID(bodyDecode.body.id)
-        break;
+        break
 
       case FB_FC.deleteData:
         dataRequest = await dataFB.deleteData(bodyDecode.body.id)
-        break;
+        break
     }
     return formatResponseDataServer(dataRequest, bodyDecode)
-
   } catch (error) {
     return new Response('You are hacker', { status: 500 })
   }
-
 }
