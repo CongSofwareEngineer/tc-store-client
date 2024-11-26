2efee97b7dfa1cf16951fa0949cd21525cc4aefd
HoDienCong <hodiencong2000@gmail.com>
2024-11-26 23:02:50 +0700

    disable console lo

--- redux/store.ts
+++ redux/store.ts
@@ -44,6 +44,7 @@ export const makeStore = () => {
           ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
         },
       }),
+    devTools: process.env.NODE_ENV === 'production',
   })
   if (isClient) {
     return storeRedux
