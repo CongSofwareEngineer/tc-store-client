94e22a12849d4a888ac29eb5007a0ef243257ddf
Cong_Bacoor <cong@bacoor.co>
2024-12-20 15:19:16 +0700

    fix page about

--- app/about/view.tsx
+++ app/about/view.tsx
@@ -11,48 +11,39 @@ import { Button, Input } from 'antd'
 import ModalDelete from '@/components/ModalDelete'
 import ClientApi from '@/services/clientApi'
 import { showNotificationError, showNotificationSuccess } from '@/utils/notification'
-// import MyBlog from '@/components/MyBlog'
-const MyBlog = dynamic(() => import('@/components/MyBlog'), { ssr: false })
+import MyBlog from '@/components/MyBlog'
+// const MyBlog = dynamic(() => import('@/components/MyBlog'), { ssr: false })
+
+const INIT_DATA = {
+  'b351b4ab-a9af-47a1-8be5-3029325fc9ab': {
+    id: 'b351b4ab-a9af-47a1-8be5-3029325fc9ab',
+    type: 'Paragraph',
+    value: [
+      {
+        id: '9fed4bba-3182-4b61-8d50-b3ddc2e02279',
+        type: 'paragraph',
+        children: [
+          {
+            text: 'New',
+          },
+        ],
+      },
+    ],
+    meta: {
+      align: 'left',
+      depth: 0,
+      order: 0,
+    },
+  },
+}
+
 const AboutScreen = ({ data }: AboutProps) => {
   const { openModalDrawer } = useModalDrawer()
   const { userData } = useUserData()
   const { translate } = useLanguage()
-  const [category, setCategory] = useState('')
+  const [category, setCategory] = useState(data?.category || '')
   const [loading, setLoading] = useState(false)
-  const [dataAbout, setDataAbout] = useState<any>(null)
-
-  useEffect(() => {
-    if (data) {
-      console.log({ data })
-
-      setCategory(data?.category)
-      setDataAbout(JSON.parse(data.des))
-    } else {
-      const initData = {
-        'b351b4ab-a9af-47a1-8be5-3029325fc9ab': {
-          id: 'b351b4ab-a9af-47a1-8be5-3029325fc9ab',
-          type: 'Paragraph',
-          value: [
-            {
-              id: '9fed4bba-3182-4b61-8d50-b3ddc2e02279',
-              type: 'paragraph',
-              children: [
-                {
-                  text: 'New',
-                },
-              ],
-            },
-          ],
-          meta: {
-            align: 'left',
-            depth: 0,
-            order: 0,
-          },
-        },
-      }
-      setDataAbout(initData)
-    }
-  }, [data])
+  const [dataAbout, setDataAbout] = useState<any>(data?.des ? JSON.parse(data.des) : INIT_DATA)
 
   const handleSubmit = async () => {
     const callBack = async () => {
