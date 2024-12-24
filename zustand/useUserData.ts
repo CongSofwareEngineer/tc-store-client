575d2d283c6737ab90fed6cab8091f6a03bfe445
Cong_Bacoor <cong@bacoor.co>
2024-12-24 11:14:40 +0700

    fix ui

--- zustand/useUserData.ts
+++ zustand/useUserData.ts
@@ -1,12 +1,15 @@
 import { persist, devtools } from 'zustand/middleware'
 import { create } from 'zustand'
 import { INIT_ZUSTAND, TYPE_ZUSTAND, ZUSTAND } from '@/constant/zustand'
+import secureLocalStorage from 'react-secure-storage'
+import { decryptData } from '@/utils/crypto'
 
 type UserDataStoreState = { [ZUSTAND.UserData]: TYPE_ZUSTAND[ZUSTAND.UserData] }
 
 type UserDataStoreActions = {
   setUserData: (nextUserData: UserDataStoreState[ZUSTAND.UserData]) => void
   reset: () => void
+  loadDataLocal: () => void
 }
 
 type UserDataStore = UserDataStoreState & UserDataStoreActions
@@ -17,6 +20,13 @@ const zustandUserData = create<UserDataStore>()(
       [ZUSTAND.UserData]: INIT_ZUSTAND[ZUSTAND.UserData],
       setUserData: (user: TYPE_ZUSTAND[ZUSTAND.UserData]) => set({ [ZUSTAND.UserData]: user }),
       reset: () => set({ [ZUSTAND.UserData]: INIT_ZUSTAND[ZUSTAND.UserData] }),
+      loadDataLocal: () => {
+        const dataSecure = secureLocalStorage.getItem(ZUSTAND.UserData)
+        if (dataSecure) {
+          const dataDecode = decryptData(dataSecure.toString())
+          set(JSON.parse(dataDecode))
+        }
+      },
     }),
     { name: ZUSTAND.UserData },
   ),
@@ -26,10 +36,12 @@ export const useUserData = () => {
   const data = zustandUserData((state) => state[ZUSTAND.UserData])
   const setUserData = zustandUserData((state) => state.setUserData)
   const reset = zustandUserData((state) => state.reset)
+  const loadDataLocal = zustandUserData((state) => state.loadDataLocal)
 
   return {
     userData: data,
     setUserData,
     reset,
+    loadDataLocal,
   }
 }
