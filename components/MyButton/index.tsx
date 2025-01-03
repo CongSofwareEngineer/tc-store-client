cf53eaf98295d5edf68941061bc59b4b92fa7af2
Cong_Bacoor <cong@bacoor.co>
2025-01-03 17:29:56 +0700

    fix ui

--- components/MyButton/index.tsx
+++ components/MyButton/index.tsx
@@ -1,20 +1,29 @@
 import { LoadingOutlined } from '@ant-design/icons'
+import { ButtonType } from 'antd/es/button'
 import React from 'react'
 type MyButton = {
   className?: string
   children?: React.ReactNode
   onClick?: () => any
   loading?: boolean
+  type?: ButtonType
 }
-const MyButton = ({ className, children, onClick, loading = false }: MyButton) => {
-  const handleClick = () => {
+const MyButton = ({
+  className,
+  children,
+  onClick,
+  loading = false,
+  type = 'default',
+}: MyButton) => {
+  const handleClick = async () => {
     if (!loading) {
-      onClick && onClick()
+      onClick && (await onClick())
     }
   }
+
   return (
     <button
-      className={`my-button flex justify-center items-center gap-2 ${className}`}
+      className={`button-${type} flex justify-center items-center gap-2 ${className}`}
       onClick={handleClick}
     >
       {loading && <LoadingOutlined />}
