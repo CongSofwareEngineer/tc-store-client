2c29e352398eda6a93960d225aaa3f3f7b4b82ce
Cong_Bacoor <cong@bacoor.co>
2025-01-24 11:50:57 +0700

    fix ui

--- utils/serverNext.ts
+++ utils/serverNext.ts
@@ -1,6 +1,3 @@
-import queryString from 'query-string'
-import { decryptData, encryptData } from './crypto'
-
 export const generateMetaBase = ({
   dataBase,
   title = null,
@@ -39,59 +36,4 @@ export const generateMetaBase = ({
     dataClone.twitter.images = image
   }
   return dataClone
-}
-
-export const pareDataClient = async (req: any): Promise<any> => {
-  const dataReq = await req.json()
-  let bodyDecode: any = decryptData(dataReq.data)
-  bodyDecode = JSON.parse(bodyDecode)
-  return bodyDecode
-}
-
-export const pareResponseDataClient = async (
-  param: any,
-  req: any
-): Promise<{ data: any; message: string }> => {
-  if (param.encode) {
-    return {
-      data: JSON.parse(decryptData(req.data?.data ?? req?.data ?? req ?? '')),
-      message: 'success',
-    }
-  }
-  return {
-    data: req.data?.data ?? req?.data ?? req ?? '',
-    message: 'success',
-  }
-}
-
-export const formatResponseDataServer = async (data: any, bodyDecode: any): Promise<Response> => {
-  const body = {
-    data: data,
-    message: 'success',
-  }
-  if (bodyDecode?.encode) {
-    body.data = encryptData(JSON.stringify(data))
-  }
-  return new Response(JSON.stringify(body), { status: 200 })
-}
-
-export const formatSearchParams = (param: any) => {
-  try {
-    if (Object.entries(param).length === 0) {
-      return ''
-    }
-    // let query = '?'
-    // Object.entries(param).forEach(([key, value], index) => {
-    //   if (index === Object.entries(param).length - 1) {
-    //     query += `${key}=${value}`
-    //   } else {
-    //     query += `${key}=${value}&`
-
-    //   }
-    // })
-    const searchPare = queryString.stringify(param, { arrayFormat: 'comma' })
-    return `?${searchPare}`
-  } catch {
-    return ''
-  }
 }
