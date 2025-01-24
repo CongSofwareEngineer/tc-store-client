bb1a6a4b02972ae64fd948ca9c8a473e85fec1db
Cong_Bacoor <cong@bacoor.co>
2025-01-24 15:10:48 +0700

    fix build

--- components/Notification/index.tsx
+++ components/Notification/index.tsx
@@ -16,8 +16,6 @@ const NotificationClient: NextPage = () => {
   const router = useRoutePage()
 
   useEffect(() => {
-    self.registration.showNotification(notificationTitle, notificationOptions)
-
     const handleAddEvent = () => {
       const handleOpenNoti = (dataMess: any) => {
         const key = `open${Date.now()}`
