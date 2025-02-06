f1a30e1fb540cf4dcb95ac0d85cbe26779cbb855
hodiencong <cong@bacoor.co>
2025-02-06 21:28:18 +0700

    new shadcn UI

--- components/ToastNoti/index.tsx
+++ components/ToastNoti/index.tsx
@@ -2,11 +2,17 @@ import useMedia from '@/hook/useMedia'
 import React from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { Button } from '../ui/button'
 
 const ToastNoti = () => {
   const { isMobile } = useMedia()
 
-  return <ToastContainer className={'mb-3'} style={{ marginTop: isMobile ? 65 : 42 }} />
+  return <>
+  <ToastContainer className={'mb-3'} style={{ marginTop: isMobile ? 65 : 42 }} />
+  <Button className='bg-green-500' >
+      sadasd
+  </Button>
+  </>
 }
 
 export default ToastNoti
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,27 +11,82 @@ const config = {
 
   prefix: '',
   theme: {
-    extend: {
-      keyframes: {
-        zoom: {
-          '0%': { transform: 'scale(0)' },
-          '100%': { transform: 'scale(1)' },
-        },
-        'spin-3': {
-          from: { transform: 'rotate(0deg)' },
-          to: { transform: 'rotate(360deg)' },
-        },
-      },
-      animation: {
-        zoom: 'zoom 0.2s ease-in-out',
-        spin3s: 'spin-3 3s infinite linear',
-      },
-      boxShadow: {
-        full: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
-      },
-    },
+  	extend: {
+  		keyframes: {
+  			zoom: {
+  				'0%': {
+  					transform: 'scale(0)'
+  				},
+  				'100%': {
+  					transform: 'scale(1)'
+  				}
+  			},
+  			'spin-3': {
+  				from: {
+  					transform: 'rotate(0deg)'
+  				},
+  				to: {
+  					transform: 'rotate(360deg)'
+  				}
+  			}
+  		},
+  		animation: {
+  			zoom: 'zoom 0.2s ease-in-out',
+  			spin3s: 'spin-3 3s infinite linear'
+  		},
+  		boxShadow: {
+  			full: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
-  darkMode: 'class',
+  darkMode: ['class', 'class'],
   // corePlugins: {
   //   preflight: false // <== disable this!
   // },
@@ -55,7 +110,8 @@ const config = {
 
       addUtilities(newUtilities)
     }),
-  ],
+      require("tailwindcss-animate")
+],
 } satisfies Config
 
 export default config
