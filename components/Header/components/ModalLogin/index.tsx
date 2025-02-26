96d0178a2e0614877f06fcf12a9eac51cf0999e6
HoDienCong <hodiencong2000@gmail.com>
2025-02-26 21:38:41 +0700

    remove autocomplete attributes from login form inputs

--- components/Header/components/ModalLogin/index.tsx
+++ components/Header/components/ModalLogin/index.tsx
@@ -57,14 +57,12 @@ const ModalLogin = () => {
           keyName='sdt'
           label={translate('userDetail.sdt')}
           placeholder={translate('userDetail.sdt')}
-          autoComplete='username'
         />
         <InputPasswordForm
           required
           label={translate('userDetail.pass')}
           placeholder={translate('userDetail.pass')}
           keyName='pass'
-          autoComplete='current-password'
           formData={formData}
         />
         <div className='flex md:flex-row justify-between w-full md:gap-0 gap-2 mb-3'>
