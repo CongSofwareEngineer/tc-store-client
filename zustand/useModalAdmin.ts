90a8298a8315ec9e683a5b0e1dd4ca762bb42807
HoDienCong <hodiencong2000@gmail.com>
2025-03-12 19:17:08 +0700

    refactor: simplify modal admin store actions and enhance modal properties

--- zustand/useModalAdmin.ts
+++ zustand/useModalAdmin.ts
@@ -1,57 +1,58 @@
-import { devtools } from 'zustand/middleware';
-import { create } from 'zustand';
-import { INIT_ZUSTAND, TYPE_ZUSTAND, ZUSTAND } from '@/constants/zustand';
+import { devtools } from 'zustand/middleware'
+import { create } from 'zustand'
+import { INIT_ZUSTAND, TYPE_ZUSTAND, ZUSTAND } from '@/constants/zustand'
 
-type ModalAdminData = TYPE_ZUSTAND[typeof ZUSTAND.ModalAdmin];
+type ModalAdminData = TYPE_ZUSTAND[typeof ZUSTAND.ModalAdmin]
 
-type ModalAdminStoreState = { [ZUSTAND.ModalAdmin]: ModalAdminData };
+type ModalAdminStoreState = { [ZUSTAND.ModalAdmin]: ModalAdminData }
 
 type ModalAdminStoreActions = {
-	setModalAdmin: (nextModalAdmin: ModalAdminStoreState[ZUSTAND.ModalAdmin]) => void;
-	openModal: (nextModalAdmin: ModalAdminStoreState[ZUSTAND.ModalAdmin]) => void;
-	closeModal: (isIconClose?: boolean) => void;
-};
+  setModalAdmin: (nextModalAdmin: ModalAdminStoreState[ZUSTAND.ModalAdmin]) => void
+  openModal: (nextModalAdmin: ModalAdminStoreState[ZUSTAND.ModalAdmin]) => void
+  closeModal: (isIconClose?: boolean) => void
+}
 
-type ModalAdminStore = ModalAdminStoreState & ModalAdminStoreActions;
+type ModalAdminStore = ModalAdminStoreState & ModalAdminStoreActions
 
 const zustandModalAdmin = create<ModalAdminStore>()(
-	devtools(
-		(set) => ({
-			[ZUSTAND.ModalAdmin]: INIT_ZUSTAND[ZUSTAND.ModalAdmin],
-			setModalAdmin: (param: ModalAdminData) => set({ [ZUSTAND.ModalAdmin]: param }),
-			openModal: (param: ModalAdminData) => {
-				set((state) => ({
-					[ZUSTAND.ModalAdmin]: {
-						showBtnClose: true,
-						...state,
-						...param,
-						open: true,
-					},
-				}));
-			},
-			closeModal: (isIconClose: boolean = false) => {
-				set((state) => {
-					if (!isIconClose) {
-						if (state?.[ZUSTAND.ModalAdmin]?.callBackAfter) {
-							state?.[ZUSTAND.ModalAdmin].callBackAfter();
-						}
-					}
-					return {
-						[ZUSTAND.ModalAdmin]: {
-							...INIT_ZUSTAND[ZUSTAND.ModalAdmin],
-							open: false,
-						},
-					};
-				});
-			},
-		}),
-		{
-			name: `zustand-${ZUSTAND.ModalAdmin}`,
-			enabled: process.env.NEXT_PUBLIC_DISABLE_DEV === undefined,
-		}
-	)
-);
+  devtools(
+    (set) => ({
+      [ZUSTAND.ModalAdmin]: INIT_ZUSTAND[ZUSTAND.ModalAdmin],
+      setModalAdmin: (param: ModalAdminData) => set({ [ZUSTAND.ModalAdmin]: param }),
+      openModal: (param: ModalAdminData) => {
+        set({
+          [ZUSTAND.ModalAdmin]: {
+            showBtnClose: true,
+            position: 'center',
+            overClickClose: true,
+            ...param,
+            open: true,
+          },
+        })
+      },
+      closeModal: (isIconClose: boolean = false) => {
+        set((state) => {
+          if (!isIconClose) {
+            if (state?.[ZUSTAND.ModalAdmin]?.callBackAfter) {
+              state?.[ZUSTAND.ModalAdmin].callBackAfter()
+            }
+          }
+          return {
+            [ZUSTAND.ModalAdmin]: {
+              ...INIT_ZUSTAND[ZUSTAND.ModalAdmin],
+              open: false,
+            },
+          }
+        })
+      },
+    }),
+    {
+      name: `zustand-${ZUSTAND.ModalAdmin}`,
+      enabled: process.env.NEXT_PUBLIC_DISABLE_DEV === undefined,
+    }
+  )
+)
 
 export const useModalAdmin = () => {
-	return zustandModalAdmin((state) => state);
-};
+  return zustandModalAdmin((state) => state)
+}
